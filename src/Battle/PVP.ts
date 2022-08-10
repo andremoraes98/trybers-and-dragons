35c816eb6c5139a6b011ad468876d9b8cb160217
Andre Moraes <andre.moraes.98@gmail.com>
2022-08-10 15:39:23 -0300

    feat: criação da classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,19 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  constructor(private player1: Fighter, private player2: Fighter) {
+    super(player1);    
+  }
+
+  public fight(): number {
+    while (this.player1.lifePoints !== -1 && this.player2.lifePoints !== -1) {
+      this.player1.attack(this.player2);
+      this.player2.attack(this.player1);
+    }
+
+    return this.player1.lifePoints === -1 ? -1 : 1;
+  }
+}
+
+export default PVP;
